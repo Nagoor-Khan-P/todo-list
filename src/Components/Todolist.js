0eb2b9d499ab4474ee0d8f707e2c97eb3f662324
Nagoor Patan <v-npatan@microsoft.com>
2022-09-10 12:06:29 +0530

    added todolist component and tasks component

---
+++ src/Components/Todolist.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import Tasks from "../modals/Tasks";
+
+export default function Todolist() {
+    const [modal,setModal]=useState(false);
+    const [status,setStatus]=useState(false);
+  return (
+    <div className="container-fluid text-center my-3 bg-secondary">
+        {/* header container */}
+      <div className="container-lg">
+        <h1 className="display-5 text-warning">Todo List Application</h1>
+        <button className="btn btn-danger my-3">Add Task</button>
+      </div>
+        {/* task container */}
+      <div className="container-lg">
+
+      </div>
+      <Tasks modal={modal}status={status}/>
+    </div>
+  );
+}
---
+++ src/modals/Tasks.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+export default function Tasks({modal,toggle}) {
+  return (
+    <div>
+        <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Do Something
+          </Button>{' '}
+          <Button color="secondary" onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  )
+}
